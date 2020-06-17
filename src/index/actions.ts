b82f43ebbc3c345c92d6c712c4fa5d258ababa36
yunruofengsheng <yunruofengsheng@126.com>
2020-06-17 20:28:57 +0800

    change

--- src/index/actions.ts
+++ src/index/actions.ts
@@ -10,16 +10,6 @@ export type isDateSelectorVisibleType = boolean
 export type departDateType = number
 export type highSpeedType = boolean
 
-export const ACTION_SET_FROM = 'SET_FROM';
-export const ACTION_SET_TO = 'SET_TO';
-export const ACTION_SET_IS_CITY_SELECTOR_VISIBLE = 'SET_IS_CITY_SELECTOR_VISIBLE';
-export const ACTION_SET_CURRENT_SELECTING_LEFT_CITY = 'SET_CURRENT_SELECTING_LEFT_CITY';
-export const ACTION_SET_CITY_DATA = 'SET_CITY_DATA';
-export const ACTION_SET_IS_LOADING_CITY_DATA = 'SET_IS_LOADING_CITY_DATA';
-export const ACTION_SET_IS_DATE_SELECTOR_VISIBLE = 'SET_IS_DATE_SELECTOR_VISIBLE';
-export const ACTION_SET_HIGH_SPEED = 'SET_HIGH_SPEED';
-export const ACTION_SET_DEPART_DATE = 'SET_DEPART_DATE';
-
 export interface initDataType {
     from: fromType
     to: toType
@@ -31,6 +21,16 @@ export interface initDataType {
     departDate: departDateType
     highSpeed: highSpeedType
 }
+
+export const ACTION_SET_FROM = 'SET_FROM';
+export const ACTION_SET_TO = 'SET_TO';
+export const ACTION_SET_IS_CITY_SELECTOR_VISIBLE = 'SET_IS_CITY_SELECTOR_VISIBLE';
+export const ACTION_SET_CURRENT_SELECTING_LEFT_CITY = 'SET_CURRENT_SELECTING_LEFT_CITY';
+export const ACTION_SET_CITY_DATA = 'SET_CITY_DATA';
+export const ACTION_SET_IS_LOADING_CITY_DATA = 'SET_IS_LOADING_CITY_DATA';
+export const ACTION_SET_IS_DATE_SELECTOR_VISIBLE = 'SET_IS_DATE_SELECTOR_VISIBLE';
+export const ACTION_SET_HIGH_SPEED = 'SET_HIGH_SPEED';
+export const ACTION_SET_DEPART_DATE = 'SET_DEPART_DATE';
 
 export interface ActionFormType {
     type: typeof ACTION_SET_FROM
